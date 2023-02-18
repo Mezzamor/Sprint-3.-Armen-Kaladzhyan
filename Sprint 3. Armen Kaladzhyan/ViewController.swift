821f5734c6f9170597669690738c2e47f28eb780
Armen Kaladzhyan <mezzamor@gmail.com>
2023-02-18 17:38:06 +0400

    Создано с нуля приложение-счетчик. При нажатии на Button прибавляется +1 значение к счетчику.

--- Sprint 3. Armen Kaladzhyan/ViewController.swift
+++ Sprint 3. Armen Kaladzhyan/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var countTextLabel: UILabel!
+    @IBOutlet weak var countStatusLabel: UILabel!
+    @IBOutlet weak var countUIButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countStatusLabel.text = "0"
+        countTextLabel.text = "Значение счетчика:\n"
     }
+    
 
-
+    private var countNumber: Int = 0
+    
+    
+    @IBAction func pushingTheButton(_ sender: Any) {
+        countNumber += 1
+        countStatusLabel.text = String(countNumber)
+    }
 }
 
